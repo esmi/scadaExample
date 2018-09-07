f8f29d45f181dd70ef56d20e178fd60e8fa196ae
evan chen <evansmile@hotmail.com>
2018-09-07 09:56:07 +0800

    bug fix.

--- example/run.sh
+++ example/run.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 SERVER_PORT=8000
+src="../vendor/esmi/scadajs/src"
+
+#check esmi/scadajs package is exist.
+if  ! ( [ -d ./src ] || [ -L ./src ] ) ; then
+    if [ -d "$src" ]; then
+        ln -s "$src" ./src
+    else
+        echo "esmi/scadajs package not exist, abort!"
+        exit
+    fi
+fi
 cat << EOF
 
 use your browser look at "http://localhost:$SERVER_PORT/"
-
 
 EOF
 php -S localhost:$SERVER_PORT
